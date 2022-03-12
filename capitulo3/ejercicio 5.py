ce61ce0aa2806a1f487e3d1b96433f968260cf0f
anicarolinacruz <accruz786@misena.edu.co>
2022-03-12 11:39:53 -0500

    cambio1
    msj

--- capitulo3/ejercicio 5.py
+++ capitulo3/ejercicio 5.py
@@ -3,4 +3,4 @@ y1=int(input("digite el valor del vector"))
 x2=int(input("digite el valor del vector"))
 y2=int(input("digite el valor del vector"))
 s=((x2-x1)**2+(y2-y1)**2)**(1/2)
-print(s)
+print(s)7
