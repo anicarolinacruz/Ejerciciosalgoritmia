b5066ddd494ed3dce5a7dc225eb2b848d98c526b
anicarolinacruz <accruz786@misena.edu.co>
2022-03-17 20:48:07 -0500

    cambios

---
+++ Nueva carpeta/cadenas.py
@@ -0,0 +1,6 @@
+n=5
+nn=55
+nnn=555
+x=n+nn+nnn
+print(x)
+print("n es igual {} y el resultado es: {}". format(n,x))
---
+++ TheChampions.py
@@ -0,0 +1,8 @@
+from ctypes.wintypes import PINT
+
+
+partidos_ganados=int(input("ingrse total de partidos ganado"))
+partidos_empatados=int(input("ingrese total "))
+partidos_perdidos=0
+puntos=(partidos_ganados*3)+(partidos_empatados*1)
+print("el total de puntos es: ",puntos)
